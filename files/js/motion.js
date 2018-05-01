604dea32df8d145d12cf6f6e559be64bac7d2d3a
FakeDoorComputing <32259974+FakeDoorComputing@users.noreply.github.com>
2018-05-01 09:15:49 +0100

    changed beta and gamma, changed name of trap flag

--- files/js/motion.js
+++ files/js/motion.js
@@ -1,9 +1,9 @@
 function get_orientation(event){
 
-  var y_value=(event.beta/100)//+1;
+  var y_value=(event.gamma/100)//+1;
   var z_value=(event.beta/100)//+1;
 
-  console.log("y_v: "+y_value+" z_v: "+z_value+" s_y: "+sen_y+" s_z: "+sen_z+" p0: "+player[0]+" p1: "+player[1])
+  console.log("y_v: "+y_value+" z_v: "+z_value+" s_y: "+sen_y+" s_z: "+sen_z+" p0: "+player[0]+" p1: "+player[1]+" wall: "+wall+" paused: "+paused+" trap: "+trap_triggered)
 
   if(wall){
     sen_y-=y_value;
@@ -11,7 +11,7 @@ function get_orientation(event){
     wall=false;
   }
 
-  if(!paused&&!trap){
+  if(!paused&&!trap_triggered){
     player[0]+=z_value;
     player[1]+=y_value;
   }
